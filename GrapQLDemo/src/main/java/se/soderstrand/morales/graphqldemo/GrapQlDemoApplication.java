c7d56a07400adcc4a218118421cbf7a830613b89
Martin Söderstrand <martinsoderstrand96@hotmail.com>
2018-05-26 14:17:43 +0200

    Created the foundation of a spring boot application using GraphQL

---
+++ GrapQLDemo/src/main/java/se/soderstrand/morales/graphqldemo/GrapQlDemoApplication.java
@@ -0,0 +1,12 @@
+package se.soderstrand.morales.graphqldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrapQlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrapQlDemoApplication.class, args);
+	}
+}
