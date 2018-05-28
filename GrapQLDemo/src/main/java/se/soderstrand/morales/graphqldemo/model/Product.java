6dd9831d9335843562fd67fdcb8715e40e6adce5
Martin Söderstrand <martinsoderstrand96@hotmail.com>
2018-05-28 21:08:12 +0200

    The graphql schema is updated to support relations between customer, salesOrder and product.

--- GrapQLDemo/src/main/java/se/soderstrand/morales/graphqldemo/model/Product.java
+++ GrapQLDemo/src/main/java/se/soderstrand/morales/graphqldemo/model/Product.java
@@ -17,6 +17,7 @@ public class Product {
     private Long id;
     private String name;
     private String category;
+    private String brand;
     @ManyToOne
     private SalesOrder salesOrder;
 }
